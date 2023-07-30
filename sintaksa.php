43cf160ebeaae7d9a2a3baff6c93a689cf736aec
cvijander <cvija85@gmail.com>
2023-07-31 00:07:21 +0200

    sintaksa

---
+++ sintaksa.php
@@ -0,0 +1,23 @@
+<?php 
+echo ("Komande u PHP se moraju zavrsiti sa : ");
+?>
+
+<?php 
+echo("Ako zelimo navodnike u tekstu moramo koristiti \"kosu crtu\"");
+?>
+
+<?php 
+//ucimo da pisemo komentare
+echo ("Ovo je jednoredni komentar");
+
+?>
+
+<?php 
+/* ovo je viseredni 
+komentar 
+*/ 
+
+echo ("Ovo je viseredni komentar.");
+
+
+?>
