67a6b51801f1143cbb9d76c7d20565683f38a903
cvijander <cvija85@gmail.com>
2023-07-31 00:00:49 +0200

    ispis

---
+++ ispis.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <title>Integrisanje PHP u HTML</title>
+</head>
+<body>
+   <div>
+       <p> Ispis nekog teksta </p>
+       <p>
+           <?php
+               echo('Ispis nekog teksta');
+            ?>
+       </p>
+   </div>
+</body>
+</html>
