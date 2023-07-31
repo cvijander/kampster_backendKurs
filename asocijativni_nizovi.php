e8f23fc28716e101baf47c3b9f89430f73b0e0d6
cvijander <cvija85@gmail.com>
2023-07-31 18:01:37 +0200

    asocijativni niz

---
+++ asocijativni_nizovi.php
@@ -0,0 +1,16 @@
+<?php 
+
+$osoba['ime'] = "Paja";
+$osoba['prezime'] = "Patak";
+
+
+$osoba123 = ['ime' => "Paja", 
+              'prezime' => "Patak",
+];
+
+  echo $osoba123['ime'];            
+
+
+
+
+?>
