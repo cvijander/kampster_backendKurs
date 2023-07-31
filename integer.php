68557d362a122e6cb943c0c3c8774570f34d1d4c
cvijander <cvija85@gmail.com>
2023-07-31 10:07:10 +0200

    integer

---
+++ integer.php
@@ -0,0 +1,31 @@
+<?php 
+$x =103;
+$y =-1098;
+$z =0;
+
+echo "Vrednost promenljive: " .$x . "| Tip promenljive: " .gettype($x). "<br/>";
+echo "Vrednost promenljive: " .$y . "| Tip promenljive: " .gettype($y) . "<br/>";
+echo "Vrednost promenljive: " . $z . "| Tip promenljive: " .gettype($z) . "<br/>";
+
+
+
+?>
+
+<?php 
+$a = 13.3;
+$b = -1098.78;
+$c = 0.1;
+
+echo "Vrednost promenljive: ".$a . " | Tip promenljive : " .gettype($a) . "<br/>";
+echo "Vrednost promenljive: ".$b . " | Tip promenljive : " .gettype($b) . "<br/>";
+echo "Vrednost promenljive: " .$c . "| Tip promenljive: " .gettype($c) . "<br/>";
+
+
+?>
+<?php 
+$Xtrue= true;
+$Yfalse = false;
+echo "Tip promenljive: ".gettype($Xtrue). "<br/>";
+echo "Tip promenljive: " .gettype($Yfalse) . "<br/>";
+
+?>
