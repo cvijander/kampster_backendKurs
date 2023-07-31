3c30efb8e153f0ae5794ad4d5f2fbd29a8780b40
cvijander <cvija85@gmail.com>
2023-07-31 18:13:23 +0200

    multi niz

---
+++ multidimenzionalni_niz.php
@@ -0,0 +1,45 @@
+<?php
+$osobe=
+[
+   "Pera Perić" =>
+       [
+           "Beograd",
+            22,
+            "PHP",
+       ],
+   "Laza Lazić" =>
+       [
+           "Kragujevac",
+            18,
+           "JavaScript",
+       ],
+
+];
+
+$rangLista=
+   [
+       0 =>
+           [
+               'Pera Perić',
+               [
+                   'poeni' => 180,
+               ]
+           ],
+       1 =>
+           [
+               'Laza Lazić',
+               [
+                   'poeni' => 100,
+               ]
+           ],
+       2 =>
+                 [
+               'Mika Mitić',
+               [
+                   'poeni' => 60,
+               ]
+           ],
+
+   ];
+
+?>
