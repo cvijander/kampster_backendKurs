049855fac98b2ed55a935e0bdc9618bc6ca64fe8
cvijander <cvija85@gmail.com>
2023-07-31 16:05:24 +0200

    nizovi

---
+++ nizovi.php
@@ -0,0 +1,19 @@
+<?php 
+$ime1="Pera";
+$ime2= "Mika";
+$ime3= "Laza";
+
+$imena = ["Pera", "Mika", "Laza"];
+
+$imena1= array("Pera","Mika", "Laza");
+
+$imena1[0] ="Pera";
+$imena1[1] = "Mika";
+$imena1[2] = "Laza";
+
+echo "Drugi clan niza: " .$imena1[1] . "<br/>";
+
+
+
+
+?>
