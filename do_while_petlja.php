ef968de66ee42eddb4af260ab1c01e41aa7eeffc
cvijander <cvija85@gmail.com>
2023-08-01 23:02:33 +0200

    do while

---
+++ do_while_petlja.php
@@ -0,0 +1,19 @@
+<?php 
+
+// do{
+//     //kod koji se izvrsava prvi put sigurno i onda sve dok je uslov tacan
+// }  while(true);
+
+
+$brojac =10;
+
+do{
+
+    echo $brojac;
+}
+ while($brojac<10);
+
+
+
+
+?>
